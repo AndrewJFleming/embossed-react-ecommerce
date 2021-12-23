268c16dba63ce06b0c241f0d1ea624745695ccbe
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-23 14:08:32 -0800

    Add Link to CategoryItem component

--- client/src/components/Categories/CategoryItem/CategoryItem.jsx
+++ client/src/components/Categories/CategoryItem/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./CategoryItem.css";
 import { Card } from "react-bootstrap";
@@ -6,11 +7,13 @@ import { Card } from "react-bootstrap";
 const CategoryItem = ({ category }) => {
   return (
     <div className="catItemWrapper">
-      <img src={category.img} />
-      <div className="catItemInfo">
-        <h5>{category.title}</h5>
-        <button>Explore</button>
-      </div>
+      <Link to={`/product-list/${category.cat}`}>
+        <img src={category.img} />
+        <div className="catItemInfo">
+          <h5>{category.title}</h5>
+          <button>Explore</button>
+        </div>
+      </Link>
     </div>
   );
 };
