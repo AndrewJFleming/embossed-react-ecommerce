235b62ca38411f5ff8e139ab107e7d26e0788efa
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-30 10:31:06 -0800

    Build out UserList component to display dummy user data

--- admin/src/pages/UserList/UserList.jsx
+++ admin/src/pages/UserList/UserList.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Table, Button } from "react-bootstrap";
+import { userRows } from "../../dummyData";
+import "./UserList.css";
 
 const UserList = () => {
-  return <div>UserList</div>;
+  const [users, setUsers] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setUsers(users.filter((item) => item._id !== id));
+  };
+
+  return (
+    <div className="userList">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Transaction Volume</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr>
+              <td>{user._id}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.status}</td>
+              <td>{user.transaction}</td>
+              <td className="d-flex justify-content-around">
+                <Link to={"/user/" + user._id}>
+                  <Button variant="dark">Edit</Button>
+                </Link>
+
+                <Button
+                  className="ml-2"
+                  variant="danger"
+                  onClick={() => handleDelete(user._id)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default UserList;
