6066bdda9ed0a680d31819e594ce50d25b3b00ac
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-12-30 10:02:40 -0800

    Build out basic NewProduct component with form and inputs with onChange handlers

--- admin/src/pages/NewProduct/NewProduct.jsx
+++ admin/src/pages/NewProduct/NewProduct.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Button } from "react-bootstrap";
+import "./NewProduct.css";
 
 const NewProduct = () => {
-  return <div>NewProduct</div>;
+  const [inputs, setInputs] = useState({});
+  const [cat, setCat] = useState([]);
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(","));
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="newProduct">
+      <h1 className="addProductTitle">New Product</h1>
+      <form className="addProductForm">
+        <div className="addProductItem">
+          <label>Image</label>
+          <input type="file" id="file" onChange={() => {}} />
+        </div>
+        <div className="addProductItem">
+          <label>Title</label>
+          <input
+            name="title"
+            type="text"
+            placeholder="title..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Description</label>
+          <input
+            name="desc"
+            type="text"
+            placeholder="description..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Price</label>
+          <input
+            name="price"
+            type="number"
+            placeholder="price..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input
+            type="text"
+            placeholder="notebooks,postcards"
+            onChange={handleCat}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Stock</label>
+          <select name="inStock" onChange={handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
+        <Button onClick={handleSubmit} className="addProductButton">
+          Create
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default NewProduct;
