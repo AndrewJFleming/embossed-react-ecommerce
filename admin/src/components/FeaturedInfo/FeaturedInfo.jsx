d88d65fdbf97cc5f65443e1bf43a2caa0c043aa0
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-01-02 16:09:08 -0800

    Hide FeaturedInfo and Chart components on Home page

--- admin/src/components/FeaturedInfo/FeaturedInfo.jsx
+++ admin/src/components/FeaturedInfo/FeaturedInfo.jsx
@@ -1,10 +1,24 @@
 import { useEffect, useState } from "react";
 
+import { userRequest } from "../../requestMethods";
 import "./FeaturedInfo.css";
 
 const FeaturedInfo = () => {
   const [income, setIncome] = useState([]);
   const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getIncome();
+  }, []);
 
   return (
     <div className="featured">
--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -6,21 +6,55 @@ import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
 import { userData } from "../../dummyData";
 import WidgetSmall from "../../components/WidgetSmall/WidgetSmall";
 import WidgetLarge from "../../components/WidgetLarge/WidgetLarge";
-// import { userRequest } from "../../requestMethods";
+import { userRequest } from "../../requestMethods";
 import "./Home.css";
 
 const Home = () => {
-  const [userStats, setUserStats] = useState([]);
+  // const [userStats, setUserStats] = useState([]);
+
+  // const MONTHS = useMemo(
+  //   () => [
+  //     "Jan",
+  //     "Feb",
+  //     "Mar",
+  //     "Apr",
+  //     "May",
+  //     "Jun",
+  //     "Jul",
+  //     "Agu",
+  //     "Sep",
+  //     "Oct",
+  //     "Nov",
+  //     "Dec",
+  //   ],
+  //   []
+  // );
+
+  // useEffect(() => {
+  //   const getStats = async () => {
+  //     try {
+  //       const res = await userRequest.get("/users/stats");
+  //       console.log(res.data);
+  //       res.data.map((item) =>
+  //         setUserStats((prev) => [
+  //           ...prev,
+  //           { name: MONTHS[item._id - 1], "Active User": item.total },
+  //         ])
+  //       );
+  //     } catch {}
+  //   };
+  //   getStats();
+  // }, [MONTHS]);
 
   return (
     <div className="home">
-      <FeaturedInfo />
-      <Chart
+      {/* <FeaturedInfo /> */}
+      {/* <Chart
         data={userStats}
         title="User Analytics"
         grid
         dataKey="Active User"
-      />
+      /> */}
       <div className="homeWidgets">
         <WidgetSmall />
         <WidgetLarge />
