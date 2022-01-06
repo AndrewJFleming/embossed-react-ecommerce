8855e8baa41b8b76893e080b45df5adcace028b8
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-01-06 15:58:31 -0800

    Add getSliderCats request to Slider component and display category data for docs with truthy isSlide values.

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./Slider.css";
 import { Carousel, Container } from "react-bootstrap";
@@ -6,25 +7,35 @@ import { sliderItems } from "../../data";
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
+  const [slides, setSlides] = useState([]);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
+  useEffect(() => {
+    const getSliderCats = async () => {
+      const res = await axios.get("/categories");
+      const isSliderProduct = res.data.filter((x) => !!x.isSlide);
+      setSlides(isSliderProduct);
+    };
+    getSliderCats();
+  }, []);
+
   return (
     <Carousel fade activeIndex={index} onSelect={handleSelect}>
-      {sliderItems.map((item) => (
+      {slides.map((slide) => (
         <Carousel.Item>
           <img
             className="d-block 
             w-100 
             carouselImage"
-            src={item.img}
+            src={slide.img}
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3>{item.title}</h3>
-            <p>{item.desc}</p>
+            <h3>{slide.title}</h3>
+            <p>{slide.desc}</p>
           </Carousel.Caption>
         </Carousel.Item>
       ))}
