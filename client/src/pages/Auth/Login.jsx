0ed3dc77daeeff6c253d2adf80d68806f7f99bf8
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-01-14 11:24:47 -0800

    Refactor Login component code to use react-bootstrap and add required checks to inputs.

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -6,21 +6,19 @@ import { useHistory } from "react-router-dom";
 
 import { signin } from "../../redux/actions/auth";
 import "./Auth.css";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Form } from "react-bootstrap";
 import ErrorPrompt from "../../shared/components/ErrorPrompt/ErrorPrompt";
 import { CLEAR_AUTH_ERROR } from "../../redux/constants/actionTypes";
 // import { bgImage } from "../../images/bgImage.jpg";
 // import { login } from "../../redux/apiCalls";
 
-const initialState = {
-  username: "",
-  password: "",
-};
-
 const Login = ({ errorStatus }) => {
   // const [username, setUsername] = useState("");
   // const [password, setPassword] = useState("");
-  const [formData, setFormData] = useState(initialState);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
   const dispatch = useDispatch();
   const history = useHistory();
   const [error, setError] = useState(false);
@@ -49,22 +47,34 @@ const Login = ({ errorStatus }) => {
     <div className="pageContainer">
       <div className="pageWrapper">
         <h2 className="pageTitle">SIGN IN</h2>
-        <form>
-          <input
-            placeholder="Username"
-            name="username"
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-          />
-          <Button onClick={handleLogin}>LOGIN</Button>
+        <Form onSubmit={handleLogin}>
+          <Form.Group className="mb-3">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              required
+              value={formData.username}
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              value={formData.password}
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button type="submit">LOGIN</Button>
           {/* {error && <h6>Error</h6>} */}
           {/* <Link to="#">Forgot you password?</Link> */}
           <Link to="/register">Register</Link>
-        </form>
+        </Form>
         {error && (
           <ErrorPrompt
             h5="Error logging in..."
