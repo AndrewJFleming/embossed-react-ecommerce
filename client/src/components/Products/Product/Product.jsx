da9f16bdb04635d1a3e37c55c07db4f887988579
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-01-19 17:21:32 -0800

    Display product title and price on Homepage

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Product.css";
+import { Card } from "react-bootstrap";
 
 const Product = ({ product }) => {
   const [hovered, setHovered] = useState(false);
@@ -9,27 +10,31 @@ const Product = ({ product }) => {
   const style = hovered ? { opacity: "1" } : {};
 
   return (
-    <div
-      className="productContainer"
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      <div className="circle"></div>
-      <img src={product.img} alt="product thumb" />
-      <div className="iconsWrapper" style={style}>
-        <div className="productIcon">
-          <i className="fas fa-shopping-basket"></i>
-        </div>
-        <div className="productIcon">
-          <Link to={`/product/${product._id}`}>
-            <i className="fas fa-search"></i>
-          </Link>
-        </div>
-        <div className="productIcon">
-          <i className="fas fa-heart"></i>
-        </div>
-      </div>
-    </div>
+    // <div
+    //   className="productContainer"
+    //   onMouseEnter={() => setHovered(true)}
+    //   onMouseLeave={() => setHovered(false)}
+    // >
+    //   <div className="circle"></div>
+    //   <img src={product.img} alt="product thumb" />
+    //   <div className="iconsWrapper" style={style}>
+    //     <div className="productIcon">
+    //       <Link to={`/product/${product._id}`}>
+    //         <h5 className="">{product.title}</h5>
+    //       </Link>
+    //       <h5 className="">&nbsp;${product.price}</h5>
+    //     </div>
+    //   </div>
+    // </div>
+    <Card className="card">
+      <Card.Img className="cardImg" variant="top" src={product.img} />
+      <Card.Body className="cardBody">
+        <Link to={`/product/${product._id}`}>
+          <Card.Title>{product.title}</Card.Title>
+        </Link>
+        <h4 className="">${product.price}</h4>
+      </Card.Body>
+    </Card>
   );
 };
 export default Product;
